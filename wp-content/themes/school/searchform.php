d7237434777f5bfa424ba3597e1891d8bd4e5756
asaduzzaman-sabuj <71424316+asaduzzaman-sabuj@users.noreply.github.com>
2020-09-21 16:01:42 +0600

    website2

--- wp-content/themes/school/searchform.php
+++
@@ -1,8 +0,0 @@
-<form class="form-group" role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
-  <div id="sbox">
-       <input type="text" value="" class="form-control"  name="s" id="s">
-       <button type="submit" id="searchsubmit">
-        <i class="icon_search"></i>
-       </button>
-  </div>
-</form>
